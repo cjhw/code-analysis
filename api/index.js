e25a97cc5540d5c9a8a17d4309faf4ebf2a87913
cjhw <1784581164@qq.com>
2023-03-19 17:19:22 +0800

    feat: finish

---
+++ api/index.js
@@ -0,0 +1,79 @@
+import fs from "fs";
+import path from "path";
+import { VUETEMPTSDIR } from "../lib/constant.js";
+import { mkDir, rmDir } from "../lib/file.js";
+import codeAnalysis from "../lib/index.js";
+
+const analysis = async function (options) {
+  if (options) {
+    if (
+      !options.scanSource ||
+      !Array.isArray(options.scanSource) ||
+      options.scanSource.length == 0
+    ) {
+      Promise.reject(new Error("error: scanSource参数不能为空"));
+      return;
+    }
+    let isParamsError = false;
+    let isCodePathError = false;
+    let unExistDir = "";
+    for (let i = 0; i < options.scanSource.length; i++) {
+      if (
+        !options.scanSource[i].name ||
+        !options.scanSource[i].path ||
+        !Array.isArray(options.scanSource[i].path) ||
+        options.scanSource[i].path.length == 0
+      ) {
+        isParamsError = true;
+        break;
+      }
+      let innerBreak = false;
+      const tempPathArr = options.scanSource[i].path;
+      for (let j = 0; j < tempPathArr.length; j++) {
+        const tempPath = path.join(process.cwd(), tempPathArr[j]);
+        if (!fs.existsSync(tempPath)) {
+          isCodePathError = true;
+          unExistDir = tempPathArr[j];
+          innerBreak = true;
+          break;
+        }
+      }
+      if (innerBreak) break;
+    }
+    if (isParamsError) {
+      Promise.reject(new Error("error: scanSource参数选项必填属性不能为空"));
+      return;
+    }
+    if (isCodePathError) {
+      Promise.reject(new Error(`error: 待分析文件目录${unExistDir}不存在`));
+      return;
+    }
+    if (!options.analysisTarget) {
+      Promise.reject(new Error("error: analysisTarget参数不能为空"));
+      return;
+    }
+  } else {
+    Promise.reject(new Error("error: 缺少options"));
+    return;
+  }
+  try {
+    // 如果temp目录已经存在，则先删除目录
+    rmDir(VUETEMPTSDIR);
+    // 如果需要扫描vue文件，创建temp目录
+    if (options.isScanVue) {
+      mkDir(VUETEMPTSDIR);
+    }
+    const { report, diagnosisInfos } = await codeAnalysis(options);
+    // 删除temp目录
+    rmDir(VUETEMPTSDIR);
+    // 返回结果
+    return Promise.resolve({
+      report: report,
+      diagnosisInfos: diagnosisInfos,
+    });
+  } catch (e) {
+    return Promise.reject(e.stack);
+  }
+};
+
+export default analysis;
---
+++ cli/index.js
@@ -0,0 +1,164 @@
+#!/usr/bin/env node
+import { Command } from "commander/esm.mjs"; // 命令行交互
+import path from "path"; // 路径操作
+import fs from "fs"; // 文件操作
+import chalk from "chalk"; // 美化输出
+import { pathToFileURL } from "url";
+
+import { writeReport, writeDiagnosisReport } from "../lib/report.js";
+import { REPORTDEFAULTDIR, VUETEMPTSDIR } from "../lib/constant.js";
+
+import { mkDir, rmDir } from "../lib/file.js";
+import codeAnalysis from "../lib/index.js";
+
+const program = new Command();
+
+program
+  .command("analysis")
+  .description("analysis code and echo report")
+  .action(async () => {
+    try {
+      const configPath = path.join(process.cwd(), "./analysis.config.js");
+      const isConfig = fs.existsSync(configPath);
+      if (isConfig) {
+        let configModule = await import(pathToFileURL(configPath));
+        let config = configModule.default;
+        if (
+          config.scanSource &&
+          Array.isArray(config.scanSource) &&
+          config.scanSource.length > 0
+        ) {
+          let isParamsError = false;
+          let isCodePathError = false;
+          let unExistDir = "";
+          for (let i = 0; i < config.scanSource.length; i++) {
+            if (
+              !config.scanSource[i].name ||
+              !config.scanSource[i].path ||
+              !Array.isArray(config.scanSource[i].path) ||
+              config.scanSource[i].path.length == 0
+            ) {
+              isParamsError = true;
+              break;
+            }
+            let innerBreak = false;
+            const tempPathArr = config.scanSource[i].path;
+            for (let j = 0; j < tempPathArr.length; j++) {
+              const tempPath = path.join(process.cwd(), tempPathArr[j]);
+              if (!fs.existsSync(tempPath)) {
+                isCodePathError = true;
+                unExistDir = tempPathArr[j];
+                innerBreak = true;
+                break;
+              }
+            }
+            if (innerBreak) break;
+          }
+          if (!isParamsError) {
+            if (!isCodePathError) {
+              if (config && config.analysisTarget) {
+                try {
+                  // 如果分析报告目录已经存在，则先删除目录
+                  rmDir(config.reportDir || REPORTDEFAULTDIR);
+                  // 如果temp目录已经存在，则先删除目录
+                  rmDir(VUETEMPTSDIR);
+                  // 如果需要扫描vue文件，创建temp目录
+                  if (config.isScanVue) {
+                    mkDir(VUETEMPTSDIR);
+                  }
+                  // 分析代码
+                  const { report, diagnosisInfos } = await codeAnalysis(config);
+                  // 输出分析报告
+                  writeReport(config.reportDir || "report", report);
+                  // 输出诊断报告
+                  writeDiagnosisReport(
+                    config.reportDir || "report",
+                    diagnosisInfos
+                  );
+                  // 删除temp目录
+                  rmDir(VUETEMPTSDIR);
+                  // 代码告警/正常退出
+                  if (
+                    config.scorePlugin &&
+                    config.alarmThreshold &&
+                    typeof config.alarmThreshold === "number" &&
+                    config.alarmThreshold > 0
+                  ) {
+                    if (
+                      report.scoreMap.score &&
+                      report.scoreMap.score < config.alarmThreshold
+                    ) {
+                      console.log(
+                        chalk.red(
+                          "\n" +
+                            "代码得分：" +
+                            report.scoreMap.score +
+                            ", 不合格"
+                        )
+                      ); // 输出代码分数信息
+                      if (report.scoreMap.message.length > 0) {
+                        // 输出代码建议信息
+                        console.log(chalk.yellow("\n" + "优化建议："));
+                        report.scoreMap.message.forEach((element, index) => {
+                          console.log(chalk.yellow(index + 1 + ". " + element));
+                        });
+                      }
+                      console.log(chalk.red("\n" + "=== 触发告警 ===" + "\n")); // 输出告警信息
+                      process.exit(1); // 触发告警错误并结束进程
+                    } else {
+                      console.log(
+                        chalk.green("\n" + "代码得分：" + report.scoreMap.score)
+                      ); // 输出代码分数信息
+                      if (report.scoreMap.message.length > 0) {
+                        // 输出代码建议信息
+                        console.log(chalk.yellow("\n" + "优化建议："));
+                        report.scoreMap.message.forEach((element, index) => {
+                          console.log(chalk.yellow(index + 1 + ". " + element));
+                        });
+                      }
+                    }
+                  } else if (config.scorePlugin) {
+                    console.log(
+                      chalk.green("\n" + "代码得分：" + report.scoreMap.score)
+                    ); // 输出代码分数信息
+                    if (report.scoreMap.message.length > 0) {
+                      // 输出代码建议信息
+                      console.log(chalk.yellow("\n" + "优化建议："));
+                      report.scoreMap.message.forEach((element, index) => {
+                        console.log(chalk.yellow(index + 1 + ". " + element));
+                      });
+                    }
+                  }
+                } catch (e) {
+                  // 删除temp目录
+                  rmDir(VUETEMPTSDIR);
+                  console.log(chalk.red(e.stack)); // 输出错误信息
+                  process.exit(1); // 错误退出进程
+                }
+              } else {
+                console.log(
+                  chalk.red("error: 配置文件中缺少必填配置项analysisTarget")
+                );
+              }
+            } else {
+              console.log(
+                chalk.red(`error: 配置文件中待分析文件目录${unExistDir}不存在`)
+              );
+            }
+          } else {
+            console.log(chalk.red("error: scanSource参数选项必填属性不能为空"));
+          }
+        } else {
+          console.log(
+            chalk.red("error: 配置文件中必填配置项scanSource不能为空")
+          );
+        }
+      } else {
+        console.log(chalk.red("error: 缺少analysis.config.js配置文件"));
+      }
+    } catch (e) {
+      console.log(chalk.red(e.stack));
+    }
+  });
+
+program.parse(process.argv);
